bae60efe0131f4ac7560ae80bdebc1f401c4d1c3
JJink <jjink@JJinkui-MacBookPro.local>
2021-04-19 16:01:47 +0900

    list for

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index() :
     #return "Hello World"
-    return render_template("home.html", data = "HA")
+    return render_template("home.html")
 
 @app.route('/about')
 def about() :
-    return render_template("about.html", hello = "gary kim")
+    return render_template("about.html")
 
 @app.route('/articles')
 def article() :
-    return render_template("articles.html", hello = "gary kim")
+    articles = Articles()
+    print(articles[0]['body'], 'articles=articles')
+    return render_template("articles.html")
 
 if __name__ == "__main__" :
     app.run()
